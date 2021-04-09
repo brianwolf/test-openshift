9895234fbfc13521ffa90b1c3033ad828198213d
Brian Lobo <brian@nalabs.io>
2021-04-09 16:16:20 -0300

    * funciona el template de configmaps y secretes * se agrega un comando al install para los que tienen buildconfig

--- templates/3-build/install.sh
+++ templates/3-build/install.sh
@@ -1,4 +1,8 @@
 TEMPLATE=build
+BUILD_CONFIG=build-bc-app1
 
 oc apply -f template.yaml
 oc new-app --template=$TEMPLATE
+
+
+oc start-build $BUILD_CONFIG
--- templates/4-secrets/install.sh
+++ templates/4-secrets/install.sh
@@ -1,4 +1,7 @@
 TEMPLATE=confmapsecret
+BUILD_CONFIG=confmapsecret-bc-app1
 
 oc apply -f template.yaml
 oc new-app --template=$TEMPLATE
+
+oc start-build $BUILD_CONFIG
